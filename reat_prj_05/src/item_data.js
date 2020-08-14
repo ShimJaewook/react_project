145b2e90f54c58f49b3cd2513eab3ff8080413b5
kayaleeeee <gayeon71057@gmail.com>
2020-08-14 11:52:13 +0900

    add item data

--- reat_prj_05/src/item_data.js
+++ reat_prj_05/src/item_data.js
@@ -1,11 +1,11 @@
 const Items = [
-    {
-        name: "",
-        price: "",
-        imgUrl: "",
-        description: "",
-        category: "",
-    }
-]
+  {
+    name: "모래성 장난감",
+    price: "25,000",
+    imgUrl: "./item_images",
+    description: "노즈워크를 위한 장난감입니다.",
+    category: "toy",
+  },
+];
 
 export default Items;
