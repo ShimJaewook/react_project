43d7fe9a0f2fa25707ae119f007f45702d980529
unknown <dongjoo3253@naver.com>
2020-08-14 17:34:19 +0900

    filterData

--- reat_prj_05/src/view/ItemView.js
+++ reat_prj_05/src/view/ItemView.js
@@ -2,27 +2,7 @@ import React, { Component } from "react";
 import { Item } from "semantic-ui-react";
 
 class ItemView extends Component {
-<<<<<<< HEAD
-    render() {
-        const { item } = this.props;
-        return (
-            <div>
-                <Item>
-                    <Item.Image size="small" src={item.imgUrl} />
-                    <Item.Content>
-                        <Item.Header as="a">{item.name}</Item.Header>
-                        <Item.Description>
-                            <p>{item.description}</p>
-                        </Item.Description>
-                        <Item.Meta>
-                            <p>{item.price}</p>
-                        </Item.Meta>
-                    </Item.Content>
-                    </Item>
-            </div>
-        )
-    }
-=======
+
   render() {
     const { item } = this.props;
     return (
@@ -44,7 +24,7 @@ class ItemView extends Component {
       </div>
     );
   }
->>>>>>> 294df451ff11459bff77da413f5a8c9230afc081
+
 }
 
 export default ItemView;
