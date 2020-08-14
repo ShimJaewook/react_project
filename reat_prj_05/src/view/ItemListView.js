294df451ff11459bff77da413f5a8c9230afc081
kayaleeeee <gayeon71057@gmail.com>
2020-08-14 17:10:57 +0900

    filterd Item list

--- reat_prj_05/src/view/ItemListView.js
+++ reat_prj_05/src/view/ItemListView.js
@@ -1,27 +1,24 @@
 import React, { Component } from "react";
 import { Item } from "semantic-ui-react";
 import ItemView from "./ItemView";
+import "./ItemView.css";
 
 class ItemListView extends Component {
   render() {
-    const ItemList = this.props.ItemList.map((item) => {
-      return (
-        <ItemView key={item.imgUrl} item={item}/>
-      );
+    const filtered = this.props.ItemList.filter((Item) => Item.class == "A");
+
+    const ItemList = filtered.map((item) => {
+      return <ItemView key={item.imgUrl} item={item} />;
     });
 
-    // let ItemList_view = ItemList.length;
-
-    // // ItemList.map((val) => {
-    // //   return <li>{val}</li>;
-    // // });
-
-    // return <div>{ItemList.length > 0 ? ItemList_view : <li>none</li>} </div>;
     return (
       <div>
-        <Item.Group>{ItemList}</Item.Group>
+        <div className="sub_img"></div>
+        <div>
+          <Item.Group>{ItemList}</Item.Group>
+        </div>
       </div>
-    )
+    );
   }
 }
 
--- reat_prj_05/src/view/ItemView.js
+++ reat_prj_05/src/view/ItemView.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Item } from "semantic-ui-react";
 
 class ItemView extends Component {
-    render() {
-        const { item } = this.props;
-        return (
-            <div>
-                <Item>
-                    <Item.Image size="tiny" src={item.imgUrl} />
-                    <Item.Content>
-                        <Item.Header as="a">{item.name}</Item.Header>
-                        <Item.Description>
-                            <p>{item.description}</p>
-                        </Item.Description>
-                        <Item.Meta>
-                            <p>{item.price}</p>
-                        </Item.Meta>
-                    </Item.Content>
-                    </Item>
-            </div>
-        )
-    }
+  render() {
+    const { item } = this.props;
+    return (
+      <div>
+        <div className>
+          <Item>
+            <Item.Image size="tiny" src={item.imgUrl} />
+            <Item.Content>
+              <Item.Header as="a">{item.name}</Item.Header>
+              <Item.Description>
+                <p>{item.description}</p>
+              </Item.Description>
+              <Item.Meta>
+                <p>{item.price}</p>
+              </Item.Meta>
+            </Item.Content>
+          </Item>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ItemView
+export default ItemView;
