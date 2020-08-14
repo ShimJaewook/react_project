dfc08337ce0f54f5175db4a26452a4f0b8a2b370
unknown <dongjoo3253@naver.com>
2020-08-14 17:26:34 +0900

    filterData

--- reat_prj_05/src/view/ItemListView.js
+++ reat_prj_05/src/view/ItemListView.js
@@ -5,29 +5,14 @@ import "./ItemView.css";
 
 class ItemListView extends Component {
   render() {
-<<<<<<< HEAD
-    const ItemList = this.props.ItemList.map((item) => {
-      return (
-        <ItemView key={item.id} item={item}/>
-      );
-    });
 
-    // let ItemList_view = ItemList.length;
-
-    // // ItemList.map((val) => {
-    // //   return <li>{val}</li>;
-    // // });
-    const temp = this.props.ItemList.filter((tempItem)=>tempItem.name === "호박 쿠키");
-    console.log(temp.price);
-    // return <div>{ItemList.length > 0 ? ItemList_view : <li>none</li>} </div>;
-=======
     const filtered = this.props.ItemList.filter((Item) => Item.class == "A");
 
     const ItemList = filtered.map((item) => {
       return <ItemView key={item.imgUrl} item={item} />;
     });
 
->>>>>>> 294df451ff11459bff77da413f5a8c9230afc081
+
     return (
       <div>
         <div className="sub_img"></div>
