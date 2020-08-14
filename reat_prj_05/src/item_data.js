eb3e74e00e5ca8d8e8f9cd5d016009d82c72f5d7
unknown <dongjoo3253@naver.com>
2020-08-14 14:28:51 +0900

    item data

--- reat_prj_05/src/item_data.js
+++ reat_prj_05/src/item_data.js
@@ -8,22 +8,5 @@ const Items = [
         category: "",
     }
 ]
-=======
-  {
-    name: "모래성 장난감",
-    price: "25,000",
-    imgUrl: "./item_images/toy_sandCastle.png",
-    description: "노즈워크를 위한 장난감입니다.",
-    category: "toy",
-  },
-  {
-    name: "호박 쿠키",
-    price: "5,000",
-    imgUrl: "./item_images/food_pumkinCookie.png",
-    description: "다이어트와 변비에 좋은 비타민A가 가득한 펌킨쿠키",
-    category: "food",
-  },
-];
->>>>>>> 145b2e90f54c58f49b3cd2513eab3ff8080413b5
 
 export default Items;
