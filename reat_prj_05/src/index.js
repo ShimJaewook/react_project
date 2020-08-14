54b4f25e1360347d6b714f57165b7f2d68d273ce
chaeuen <xiromjr@kookmin.ac.kr>
2020-08-15 00:32:44 +0900

    add Jumbotron

--- reat_prj_05/src/index.js
+++ reat_prj_05/src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import ItemStore from "./store/ItemStore";
 import { Provider } from "mobx-react";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(
   <Provider ItemStore={ItemStore}>
     <App />
--- reat_prj_05/src/view/MainImageView.js
+++ reat_prj_05/src/view/MainImageView.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+// import { Image } from 'semantic-ui-react'
+import { Jumbotron, Container } from 'reactstrap';
 
 class MainImageView extends Component {
 
     render() {
         return (
+            // <Image src='/src_images/driving_dog.png' width="100%" height="50px"/>
+        
             <div>
-                
+                <Jumbotron fluid>
+                    <Container fluid>
+                        <img src='/src_images/driving_dog.png' />
+                    </Container>
+                </Jumbotron>
             </div>
         )
     }
