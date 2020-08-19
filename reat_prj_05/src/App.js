6a63a81ebad3842982d548ab233d9028b0ced690
kayaleeeee <gayeon71057@gmail.com>
2020-08-19 16:44:24 +0900

    Edit main page render

--- reat_prj_05/src/App.js
+++ reat_prj_05/src/App.js
@@ -1,19 +1,12 @@
-import React from "react"
-import ItemListContainer from "./container/ItemListContainer"
-import LoginContainer from "./container/LoginContainer"
+import React from "react";
+import ItemListContainer from "./container/ItemListContainer";
+import LoginContainer from "./container/LoginContainer";
 
-<<<<<<< HEAD
 import MainImageContainer from "./container/MainImageContainer";
 import ItemAllMain from "./view/menu/ItemAllMain";
 import Selected_item_Container from "./container/Selected_item_Container";
 import Footer from "./view/menu/Footer";
 import MiddleContentContainer from "./container/MiddleContentContainer";
-=======
-import MainImageContainer from "./container/MainImageContainer"
-import ItemAllMain from "./view/menu/ItemAllMain"
-import Selected_item_Container from "./container/Selected_item_Container"
-import MiddleContentView from "./view/MiddleContentView"
->>>>>>> 89c5a8f3e0b7ff3d4cb36a05fca3f07f2c448175
 
 function App() {
   return (
@@ -22,16 +15,11 @@ function App() {
       <MainImageContainer />
 
       <ItemListContainer />
-      <MiddleContentContainer />
-      <ItemAllMain />
-<<<<<<< HEAD
 
+      <ItemAllMain />
       <Footer />
-=======
-      <MiddleContentView />
->>>>>>> 89c5a8f3e0b7ff3d4cb36a05fca3f07f2c448175
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
