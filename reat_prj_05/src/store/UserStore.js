ec402606d2defdcfb8fc6dacfd8706f0cb9f02d3
unknown <dongjoo3253@naver.com>
2020-08-19 18:38:51 +0900

    pull

--- reat_prj_05/src/store/UserStore.js
+++ reat_prj_05/src/store/UserStore.js
@@ -1,15 +1,16 @@
-import {observable, action} from "mobx";
-import Users from "../user_data";
+import { observable, action } from "mobx"
+import Users from "../user_data"
 
 class UserStore {
   @observable
-  users = Users;
+  users = Users
 
   @observable
-  user = {};
+  user = {}
 
- 
-   //Mobx state를 변경할 함수에 @action 데코레이터를 사용하여 선언 
-   
+  @observable
+  login = false
+
+  //Mobx state를 변경할 함수에 @action 데코레이터를 사용하여 선언
 }
-export default new UserStore();
+export default new UserStore()
