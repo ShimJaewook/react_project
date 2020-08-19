737073fbd9d0b4699579e6772a79b26390f2f72f
chaeuen <xiromjr@kookmin.ac.kr>
2020-08-19 15:50:50 +0900

    temp

--- reat_prj_05/src/App.js
+++ reat_prj_05/src/App.js
@@ -5,11 +5,8 @@ import LoginContainer from "./container/LoginContainer";
 import MainImageContainer from "./container/MainImageContainer";
 import ItemAllMain from "./view/menu/ItemAllMain";
 import Selected_item_Container from "./container/Selected_item_Container";
-<<<<<<< HEAD
 import Footer from "./view/menu/Footer";
-=======
 import MiddleContentView from "./view/MiddleContentView";
->>>>>>> f617a9ff7f757d74bb4b7ac22cfa988e0fbd08d1
 
 function App() {
   return (
@@ -18,11 +15,8 @@ function App() {
       <MainImageContainer />
       <ItemListContainer />
       <ItemAllMain />
-<<<<<<< HEAD
-      <Footer />
-=======
       <MiddleContentView />
->>>>>>> f617a9ff7f757d74bb4b7ac22cfa988e0fbd08d1
+      <Footer />
     </div>
   );
 }
