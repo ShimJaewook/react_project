69d41cd3397091699e07a9701a25030923f2baa4
kayaleeeee <gayeon71057@gmail.com>
2020-08-19 17:58:39 +0900

    nav bar css & insert Logo

--- reat_prj_05/src/App.js
+++ reat_prj_05/src/App.js
@@ -1,57 +1,32 @@
-<<<<<<< HEAD
-import React from "react";
+import React, { Component } from "react";
 import ItemListContainer from "./container/ItemListContainer";
 import LoginContainer from "./container/LoginContainer";
 
-=======
-import React, { Component } from "react"
-import ItemListContainer from "./container/ItemListContainer"
-import LoginContainer from "./container/LoginContainer"
->>>>>>> ed59289cac99c5bbf0d51af6a5d72d93a077c446
 import MainImageContainer from "./container/MainImageContainer";
 import ItemAllMain from "./view/menu/ItemAllMain";
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Footer from "./view/menu/Footer";
 import MiddleContentContainer from "./container/MiddleContentContainer";
-<<<<<<< HEAD
-=======
-import Selected_item_Container from "./container/Selected_item_Container"
-import {observer, inject} from "mobx-react";
->>>>>>> ed59289cac99c5bbf0d51af6a5d72d93a077c446
+import Selected_item_Container from "./container/Selected_item_Container";
+import { observer, inject } from "mobx-react";
 
 @inject("StateStore")
 @observer
 class App extends Component {
-  
-  
-  render () {
-    const state = this.props.StateStore.getState; 
+  render() {
+    const state = this.props.StateStore.getState;
 
-<<<<<<< HEAD
-      <ItemListContainer />
-
-      <ItemAllMain />
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
-=======
     return (
       <div>
         <LoginContainer />
 
-        {state==="home" && <MainImageContainer /> }
-        {state==="home" && <ItemListContainer /> }
-        {state==="home" && <MiddleContentContainer /> }
-
-        {state==="allItem" && <ItemAllMain /> }
+        {state === "home" && <MainImageContainer />}
+        {state === "home" && <ItemListContainer />}
+        {/* {state === "home" && <MiddleContentContainer />} */}
+        {state === "allItem" && <ItemAllMain />}
 
         <Footer />
       </div>
-    )
+    );
   }
 }
-export default App
->>>>>>> ed59289cac99c5bbf0d51af6a5d72d93a077c446
+export default App;
--- reat_prj_05/src/view/LoginView.js
+++ reat_prj_05/src/view/LoginView.js
@@ -1,209 +1,175 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Form, Button, Input, Modal, Header, Image } from "semantic-ui-react";
 import "./LoginView.css";
-import "./HeaderMenu.css";
+import "./LoginView.scss";
 import "./DIY_item";
 class LoginView extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            open: false,
-            visible1: true,
-            visible2: true,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      visible1: true,
+      visible2: true,
+    };
+  }
 
-    changeView = (e) => {
-        if(this.state.visible1) 
-            this.setState({visible1: !this.state.visible1});
-        else if(this.state.visible1===false)
-            this.setState({visible2: !this.state.visible2});
+  changeView = (e) => {
+    if (this.state.visible1) this.setState({ visible1: !this.state.visible1 });
+    else if (this.state.visible1 === false)
+      this.setState({ visible2: !this.state.visible2 });
+  };
+
+  scrollView = (e) => {
+    const menuHeight = document.querySelector(".header_menu");
+    if (e.value === document.getElementById("menu-01").value) {
+      const location = document.querySelector("#menu-01").offsetTop; //해당 id를 갖는 요소의 top 위치(마진 값까지 포함)를 가져올 수 있다.
+      window.scrollTo({
+        //직접 태그의 위치로 스크롤이동
+        top: location - menuHeight,
+        behavior: "smooth",
+      });
+    }
+    if (e.value === document.getElementById("menu-02").value) {
+      const location = document.querySelector("#sub_img id").offsetTop; //해당 id를 갖는 요소의 top 위치(마진 값까지 포함)를 가져올 수 있다.
+      window.scrollTo({
+        //직접 태그의 위치로 스크롤이동
+        top: location - menuHeight,
+        behavior: "smooth",
+      });
+    }
+    if (e.value === document.getElementById("menu-03").value) {
+      const location = document.querySelector("#menu-03").offsetTop; //해당 id를 갖는 요소의 top 위치(마진 값까지 포함)를 가져올 수 있다.
+      window.scrollTo({
+        //직접 태그의 위치로 스크롤이동
+        top: location - menuHeight,
+        behavior: "smooth",
+      });
+    }
+    if (e.value === document.getElementById("menu-04").value) {
+      const location = document.querySelector("#menu-04").offsetTop; //해당 id를 갖는 요소의 top 위치(마진 값까지 포함)를 가져올 수 있다.
+      window.scrollTo({
+        //직접 태그의 위치로 smooth하게 스크롤이동
+        top: location - menuHeight,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  render() {
+    const users = this.props.users;
+
+    const rootUser = {
+      id: users[0].id,
+      pw: users[0].password,
     };
 
-    scrollView = (e) => {
-        const menuHeight = document.querySelector(".header_menu");
-        if(e.value===document.getElementById("menu-01").value) {
-           
-            const location=document.querySelector("#menu-01").offsetTop; //해당 id를 갖는 요소의 top 위치(마진 값까지 포함)를 가져올 수 있다.
-            window.scrollTo({
-                //직접 태그의 위치로 스크롤이동
-                top:location - menuHeight,    
-                behavior: 'smooth',
-        })}
-        if(e.value===document.getElementById("menu-02").value) {
-               
-            const location=document.querySelector("#sub_img id").offsetTop; //해당 id를 갖는 요소의 top 위치(마진 값까지 포함)를 가져올 수 있다.
-            window.scrollTo({
-                //직접 태그의 위치로 스크롤이동
-                top:location - menuHeight,    
-                behavior: 'smooth',
-        })}
-        if(e.value===document.getElementById("menu-03").value) {
-               
-            const location=document.querySelector("#menu-03").offsetTop; //해당 id를 갖는 요소의 top 위치(마진 값까지 포함)를 가져올 수 있다.
-            window.scrollTo({
-                //직접 태그의 위치로 스크롤이동
-                top:location - menuHeight,    
-                behavior: 'smooth',
-        })}
-        if(e.value===document.getElementById("menu-04").value) {
-               
-            const location=document.querySelector("#menu-04").offsetTop; //해당 id를 갖는 요소의 top 위치(마진 값까지 포함)를 가져올 수 있다.
-            window.scrollTo({
-                //직접 태그의 위치로 smooth하게 스크롤이동
-                top:location - menuHeight,    
-                behavior: 'smooth',
-        })}
-    }
-
-        
-    render() {
-        const users = this.props.users;
-
-        const rootUser = {
-            id: users[0].id,
-            pw:users[0].password,
-        }
-
-        return (
-            
-            <div className="top-header">
-                <div>
-                           {/* {this.state.visible1 && */}
-                {/* // <Button onClick = {this.changeView}>로그인</Button> */}
-                {/* <Modal className="login-modal"
-                    onClose={() => this.setState({ open: false })}
-                    onOpen={() => this.setState({ open: true })}
-                    open={this.state.open}
-                    trigger={<Button className="btn">로그인</Button>}>
-
-                    <Modal.Header>로그인 팝업띄우기</Modal.Header>
-                    <Modal.Content>
-                        <Modal.Description>
-                            <div>
-                                <Input type="text" id="userid" placeholder='아이디' />
-                                &nbsp;&nbsp;
-                                <Input type="password" id="password"  placeholder='비밀번호' />  
-                                &nbsp;&nbsp;
-                            </div>
-                        </Modal.Description>
-                    </Modal.Content>
-                    <Modal.Actions>
-                        <Button id="btn" onClick={(id)=>{ 
-                        if(rootUser.id !== document.getElementById("userid").value) {
-                            alert('아이디가 틀렸습니다.');
-                        }
-                    
-                        if(rootUser.pw !== document.getElementById("password").value){
-                            alert('비밀번호가 틀렸습니다');
-                        }
-                        
-                        if(rootUser.pw===document.getElementById("password").value
-                            && rootUser.id===document.getElementById("userid").value){
-                            this.changeView();
-                            alert('로그인 되었습니다!');
-                            this.setState({open:false});
-                        }}  
-                        }>로그인
-                        </Button>
-                    </Modal.Actions>
-            </Modal> */}
-            {/* } */}
-            
-            {/* <img src="./logo_image/puppy_logo.jpg" id="header_logo_pc" width="80px" height="120%" alt="puppy_logo"></img> 
-            {!this.state.visible1 && this.state.visible2 && <div>
-                <div>
-                <Input type="text" id="userid" placeholder='아이디' />
-                &nbsp;&nbsp;
-                <Input type="password" id="password"  placeholder='비밀번호' />  
-                &nbsp;&nbsp;
-                <Button id="btn" onClick={(id,)=>{ 
-                  if(rootUser.id !== document.getElementById("userid").value) {
-                      alert('아이디가 틀렸습니다.');
-                  }
-            
-                if(rootUser.pw !== document.getElementById("password").value){
-                    alert('비밀번호가 틀렸습니다');
-               
-                }
-            
-                if(rootUser.pw===document.getElementById("password").value
-                     && rootUser.id===document.getElementById("userid").value){
-                        this.changeView();
-                        alert('로그인 되었습니다!');
-                        
-                        
-                }}
-            }>로그인
-            </Button> */}
-            {/* </div> */}
-        
-        </div> 
+    return (
+      <div className="top-header">
+        <div></div>
         <div class="header_content">
-            <div class="header_menu">
-                <ul class="liMenu">
-                    <li>  
-                        <a href="#" id="menu-02" onClick={()=>this.props.changeState("home")}><span>메인으로</span></a>
-                    </li> 
-                    <li>
-                        <a onClick={()=>this.props.changeState("home")} href="#subscription item id" id="menu-01" ><span>구독상품</span></a>
-                    </li>
-                    <li>  
-                        <a href="#" id="menu-03" onClick={()=>this.props.changeState("allItem")}><span>전체 상품보기</span></a>
-                    </li>
-                    <li> 
-                        <a href="#" id="menu-04"><span>상단으로</span></a>
-                    </li>
-                    <Modal className="login-modal"
-                    onClose={() => this.setState({ open: false })}
-                    onOpen={() => this.setState({ open: true })}
-                    open={this.state.open}
-                    trigger={<Button className="btn">로그인</Button>}>
-
-                    <Modal.Header>로그인 팝업띄우기</Modal.Header>
-                    <Modal.Content>
-                        <Modal.Description>
-                            <div>
-                                <Input type="text" id="userid" placeholder='아이디' />
-                                &nbsp;&nbsp;
-                                <Input type="password" id="password"  placeholder='비밀번호' />  
-                                &nbsp;&nbsp;
-                            </div>
-                        </Modal.Description>
-                    </Modal.Content>
-                    <Modal.Actions>
-                        <Button id="btn" onClick={(id)=>{ 
-                        if(rootUser.id !== document.getElementById("userid").value) {
-                            alert('아이디가 틀렸습니다.');
-                        }
-                    
-                        if(rootUser.pw !== document.getElementById("password").value){
-                            alert('비밀번호가 틀렸습니다');
-                        }
-                        
-                        if(rootUser.pw===document.getElementById("password").value
-                            && rootUser.id===document.getElementById("userid").value){
-                            this.changeView();
-                            alert('로그인 되었습니다!');
-                            this.setState({open:false});
-                        }}  
-                        }>로그인
-                        </Button>
-                    </Modal.Actions>
-            </Modal>
-                    <li>
-
-                    </li>
-                </ul>
+          <div class="header_menu">
+            <div className="logo">
+              <img src="/logo_image/LOGO.png" />
+              <h1> 멍토피아</h1>
             </div>
-            
-           
-            {!this.state.visible2 && <div className="welcome"> 박민재님 안녕하세요!
-        </div>}
+            <ul class="liMenu">
+              <li>
+                <a
+                  href="#"
+                  id="menu-02"
+                  onClick={() => this.props.changeState("home")}
+                >
+                  <span>메인으로</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={() => this.props.changeState("home")}
+                  href="#subscription item id"
+                  id="menu-01"
+                >
+                  <span>구독상품</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  id="menu-03"
+                  onClick={() => this.props.changeState("allItem")}
+                >
+                  <span>전체 상품보기</span>
+                </a>
+              </li>
+              <li>
+                <a href="#" id="menu-04">
+                  <span>상단으로</span>
+                </a>
+              </li>
+              <Modal
+                className="login-modal"
+                onClose={() => this.setState({ open: false })}
+                onOpen={() => this.setState({ open: true })}
+                open={this.state.open}
+                trigger={<Button className="btn">로그인</Button>}
+              >
+                <Modal.Header>로그인 팝업띄우기</Modal.Header>
+                <Modal.Content>
+                  <Modal.Description>
+                    <div>
+                      <Input type="text" id="userid" placeholder="아이디" />
+                      &nbsp;&nbsp;
+                      <Input
+                        type="password"
+                        id="password"
+                        placeholder="비밀번호"
+                      />
+                      &nbsp;&nbsp;
+                    </div>
+                  </Modal.Description>
+                </Modal.Content>
+                <Modal.Actions>
+                  <Button
+                    id="btn"
+                    onClick={(id) => {
+                      if (
+                        rootUser.id !== document.getElementById("userid").value
+                      ) {
+                        alert("아이디가 틀렸습니다.");
+                      }
+
+                      if (
+                        rootUser.pw !==
+                        document.getElementById("password").value
+                      ) {
+                        alert("비밀번호가 틀렸습니다");
+                      }
+
+                      if (
+                        rootUser.pw ===
+                          document.getElementById("password").value &&
+                        rootUser.id === document.getElementById("userid").value
+                      ) {
+                        this.changeView();
+                        alert("로그인 되었습니다!");
+                        this.setState({ open: false });
+                      }
+                    }}
+                  >
+                    로그인
+                  </Button>
+                </Modal.Actions>
+              </Modal>
+              <li></li>
+            </ul>
+          </div>
+
+          {!this.state.visible2 && (
+            <div className="welcome"> 박민재님 안녕하세요!</div>
+          )}
         </div>
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default LoginView;
