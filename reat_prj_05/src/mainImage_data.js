25d01bdce15cd13c802c99969d309607e99d903b
kayaleeeee <gayeon71057@gmail.com>
2020-08-20 12:55:49 +0900

    final with main pages

--- reat_prj_05/src/mainImage_data.js
+++ reat_prj_05/src/mainImage_data.js
@@ -1,19 +1,19 @@
 const MainImages = [
     {
         id: 1,
-        imgUrl: "./main_images/sample1.png"
+        imgUrl: "./main_images/main2.png"
     },
-    {
-        id: 2,
-        imgUrl: "./main_images/sample2.png"
-    },
-    {
-        id: 3,
-        imgUrl: "./main_images/sample3.png"
-    },
+    // {
+    //     id: 2,
+    //     imgUrl: "./main_images/sample2.png"
+    // },
+    // {
+    //     id: 3,
+    //     imgUrl: "./main_images/sample3.png"
+    // },
     {
         id: 4,
-        imgUrl: "./main_images/running_dog.png"
+        imgUrl: "./main_images/main1.png"
     }
 ]
 
